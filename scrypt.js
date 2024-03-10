44d1fc7a68894fa7762f565cef9140c429a16e2c
Dmytro Horbatov <4messijess4@gmail.com>
2024-03-11 00:51:17 +0100

    double open load window fix, two game modes and minors

--- scrypt.js
+++ scrypt.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const myForm = document.querySelector("#myForm");
-    const inpFile = document.querySelector("#inpFile");
-    const inpFileLabel = document.querySelector("#inpFileLabel");
-    const card = document.querySelector(".card");
-    const nextButton = document.querySelector(".next");
-    const frontWord = document.querySelector('.card-front-content h1');
-    const backWord  = document.querySelector('.card-back-content h1');
-
-    inpFileLabel.addEventListener("click", function() {
-        inpFile.click();
-    });
+    const myForm = document.querySelector("#myForm"),
+          inpFile = document.querySelector("#inpFile"),
+          card = document.querySelector(".card"),
+          nextButton = document.querySelector(".next"),
+          frontWord = document.querySelector('.card-front-content h1'),
+          backWord = document.querySelector('.card-back-content h1'),
+          startArea = document.querySelector('.start-area'),
+          buttons = document.querySelectorAll('.btns'),
+          starButton = document.querySelector('.file-btn'),
+          frontBackground = document.querySelector('.card-front'),
+          backBackground = document.querySelector('.card-back');
 
     myForm.addEventListener("submit", handleFormSubmit);
     nextButton.addEventListener("click", handleNextButtonClick);
@@ -18,6 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         const formData = new FormData();
         formData.append("inpFile", inpFile.files[0]);
+        
+        starButton.style.display = "block";
         getWordsFromFileWithWords(formData);
     }
 
@@ -31,13 +33,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 const wordsArray = fileContent.split(/\s*[\n-]+\s*/);
 
                 myForm.style.display = "none";
-                card.style.display = "block";
+                startArea.style.display = "block";
 
                 addWordsToArray(wordsArray);
             };
             reader.readAsText(file);
         } else {
-            console.error("Файл не был передан");
+            console.error("Something went wrong");
         }
     }
 
@@ -64,8 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
 
-        returnFirstWords(dict);
-        returnWords(dict);
+        chosenMode(dict);
     }
 
     function returnWords(dict) {
@@ -76,14 +77,17 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function returnFirstWords(dict) {          
-        frontWord.innerHTML = `${dict.latin.words[0]}`;
-        backWord.innerHTML = `${dict.cyrillic.words[0]}`;
+        const randomIndex = Math.floor(Math.random() * dict.latin.words.length);
+        frontWord.textContent = dict.latin.words[randomIndex];
+        backWord.textContent = dict.cyrillic.words[randomIndex];
     }
 
     function returnRandomWordFromDict(dict) {
-        let randomIndex = Math.floor(Math.random() * dict.latin.words.length);
-        let randomLang = Math.round(Math.random());
-        let randomWord, translationOfRandomElement;
+        let randomIndex = Math.floor(Math.random() * dict.latin.words.length),
+            randomLang = Math.round(Math.random()),
+            // randomLang = 0,
+            randomWord, 
+            translationOfRandomElement;
 
         if (randomLang === 0) {
             randomWord = dict.latin.words[randomIndex];
@@ -97,24 +101,49 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function renderWords(words) {
-        card.innerHTML = `
-            <div class="card-horizontal">
-                <div class="card-front"> 
-                    <article class="card-front-content">
-                        <h1>${words[0]}</h1>
-                    </article>
-                </div>
-                <div class="card-back card-back-hr">
-                    <article class="card-back-content">
-                        <h1>${words[1]}</h1>
-                    </article>
-                </div>
-            </div>
-        `;
+        setAnotherBackgrounds();
+        frontWord.textContent = words[0];
+        backWord.textContent = words[1];
     }
 
     function handleNextButtonClick() {
         const words = returnRandomWordFromDict(dict);
         renderWords(words);
     }
+
+    function chosenMode(dict) {
+        buttons[0].addEventListener("click", function() {
+            startArea.style.display = "none";
+            card.style.display = "block";
+            returnFirstWords(dict);
+            returnWords(dict);
+        });
+
+        buttons[1].addEventListener("click", function() {
+            console.log("Clicked on the second version button");
+        });
+    }
+
+    function setAnotherBackgrounds() {
+        const colors = getRandomColors();
+        frontBackground.style.backgroundColor = colors[0];
+        backBackground.style.backgroundColor = colors[1];
+    }
+
+    function getRandomColors() {
+        const colors = [
+        '#4f4a2a', '#b00b1e', '#3f0e07', 
+        '#4f4a2a', '#000080', '#801818', 
+        '#556b2f', '#672c47', '#2c4767', 
+        '#6f4e37', '#81655F', '#000000', 
+        '#581845', '#900C3F', '#614751'];
+
+        const index1 = Math.floor(Math.random() * colors.length);
+        let index2;
+        do {
+          index2 = Math.floor(Math.random() * colors.length);
+        } while (index2 === index1);
+      
+        return [colors[index1], colors[index2]];
+      }
 });
